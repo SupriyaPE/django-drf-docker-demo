6738dbdf53278b631319f0ea71e78873f1c1994c
SupriyaPE <supriyape2002@gmail.com>
2026-01-12 02:13:59 +0530

    Fix Github

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import hello
+from .views import signup_view, login_view, profile_view, logout_view
 
 urlpatterns = [
-    path('hello/', hello),
+    path("signup/", signup_view, name="signup"),
+    path("login/", login_view, name="login"),
+    path("profile/", profile_view, name="profile"),
+    path("logout/", logout_view, name="logout"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-@api_view(['GET'])
-def hello(request):
-    return Response({"message": "Hello Django Docker"})
+def signup_view(request):
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(
+            username=email,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        return redirect("login")
+
+    return render(request, "signup.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = authenticate(username=email, password=password)
+        if user:
+            login(request, user)
+            return redirect("profile")
+
+    return render(request, "login.html")
+
+
+@login_required(login_url="login")
+def profile_view(request):
+    return render(request, "profile.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
