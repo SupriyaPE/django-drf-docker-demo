fc4870defd7e22c077673ed6797d1e376c53c3c9
SupriyaPE <supriyape2002@gmail.com>
2026-01-12 00:54:51 +0530

    Fix mysqlclient build,Docker working

---
+++ README.md
@@ -0,0 +1,12 @@
+# Django DRF Docker Demo
+
+## Tech Stack
+- Django
+- Django REST Framework
+- Docker
+- Docker Compose
+
+## How to Run (Local)
+
+```bash
+docker-compose up --build
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,13 +74,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': 'app_password',
-        'HOST': 'db',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '3306',
     }
 }
